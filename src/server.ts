bcb03e2e153283433f22ffd614a5a2749a7a2c08
Jérémy <jeremy.vincent@ynov.com>
2017-12-15 11:16:56 +0100

    fix the fix cors #lol

--- src/server.ts
+++ src/server.ts
@@ -116,7 +116,7 @@ export class Server {
       allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
       credentials: true,
       methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-      origin: '/',
+      origin: '*',
       preflightContinue: false,
     }
 
